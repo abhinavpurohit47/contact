c7860e2fa0a24cd8c6e770abd402ef94eb2bc06f
abhinavpurohit47 <purohitabhinav01@gmail.com>
2022-09-26 18:04:55 +0530

    💄 Tailwindcss Added

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '../index.css'
+
+function App() {
+  return (
+    <div className='text-3xl underline font-bold'> 
+      Hello
+    </div>
+  );
+}
+
+export default App;
